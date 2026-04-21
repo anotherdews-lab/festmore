7d93f6bd70af84c247dc43bc83d8ffdc0b6f275e
dews <gha44ar@aol.com>
2026-04-22 01:16:13 +0200

    Use resend.dev sender while domain verifies

--- cron.js
+++ cron.js
@@ -9,7 +9,7 @@ async function sendEmail(to, subject, html) {
     const { Resend } = require('resend');
     const resend = new Resend(process.env.RESEND_API_KEY);
     await resend.emails.send({
-      from: 'Festmore Newsletter <contact@festmore.com>',
+      from: 'Festmore <onboarding@resend.dev>',
       to, subject, html
     });
     return true;
